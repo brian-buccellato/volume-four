580979d7a62229da0c0af8b8dafbb16c0ebff4fa
brian buccellato <brianjbuccellato@gmail.com>
2018-12-20 11:20:44 -0500

    config object for firebase integration

---
+++ src/config/firebase-config.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/storage";
+
+const config = {
+  apiKey: "AIzaSyCykiu0fT-OmA0aab9d2A7YB1OoSCv0iVY",
+  authDomain: "volume-iv.firebaseapp.com",
+  databaseURL: "https://volume-iv.firebaseio.com",
+  projectId: "volume-iv",
+  storageBucket: "volume-iv.appspot.com",
+  messagingSenderId: "639155267070"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
